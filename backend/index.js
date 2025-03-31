0ce29d3546e7a5465f48fffcd0bbc56226bf9a7c
JungYiryung <yi_ryung@naver.com>
2025-03-31 23:15:36 +0900

    Fix: https로만 요청받는것으로 수정

--- backend/index.js
+++ backend/index.js
@@ -29,13 +29,13 @@ app.use(
 );
 
 app.use(
-  express.static("/Users/jung-yiryung/Desktop/buddyChat_demo_v2/frontend")
-  // express.static("/root/BuddyChat-backend/frontend")
+  // express.static("/Users/jung-yiryung/Desktop/buddyChat_demo_v2/frontend")
+  express.static("/root/BuddyChat-backend/frontend")
 );
 app.get("/", (req, res) => {
   res.sendFile(
-    "/Users/jung-yiryung/Desktop/buddyChat_demo_v2/frontend/chatRoom.html"
-    // "/root/BuddyChat-backend/frontend/chatRoom.html"
+    // "/Users/jung-yiryung/Desktop/buddyChat_demo_v2/frontend/chatRoom.html"
+    "/root/BuddyChat-backend/frontend/chatRoom.html"
   );
 });
 
