14becb59820238f2f204bd9ea0e5990aecedc4c8
JungYiryung <yi_ryung@naver.com>
2025-04-01 01:38:21 +0900

    Test: https 뺌

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const options = {
 };
 
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(options, app);
+// const httpsServer = https.createServer(options, app);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -33,12 +33,12 @@ app.get("/", (req, res) => {
 
 // express.static("/Users/jung-yiryung/Desktop/buddyChat_demo_v2/frontend")
 // "/Users/jung-yiryung/Desktop/buddyChat_demo_v2/frontend/chatRoom.html"
-app.use((req, res, next) => {
-  if (!req.secure) {
-    return res.redirect("https://" + req.headers.host + req.url);
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (!req.secure) {
+//     return res.redirect("https://" + req.headers.host + req.url);
+//   }
+//   next();
+// });
 
 const io = new Server(httpServer, {
   cors: {
@@ -48,13 +48,13 @@ const io = new Server(httpServer, {
   },
 });
 
-const ioSecure = new Server(httpsServer, {
-  cors: {
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST"],
-  },
-});
+// const ioSecure = new Server(httpsServer, {
+//   cors: {
+//     origin: "*",
+//     credentials: true,
+//     methods: ["GET", "POST"],
+//   },
+// });
 
 const socketHandler = (socket, ioInstance) => {
   console.log("개인소켓 연결됨.", socket.id);
@@ -64,7 +64,7 @@ const socketHandler = (socket, ioInstance) => {
 io.on("connection", (socket) => {
   socketController(socket, io);
 });
-ioSecure.on("connection", (socket) => socketHandler(socket, ioSecure));
+// ioSecure.on("connection", (socket) => socketHandler(socket, ioSecure));
 
 const currentTime = new Date().toString();
 // server.listen(process.env.PORT, () => {
@@ -74,6 +74,6 @@ const currentTime = new Date().toString();
 httpServer.listen(80, () => {
   console.log(`80포트에서 ${currentTime}현재 웹서버 실행중 `);
 });
-httpsServer.listen(443, () => {
-  console.log(`443포트에서 ${currentTime}현재 웹서버 실행중 `);
-});
+// httpsServer.listen(443, () => {
+//   console.log(`443포트에서 ${currentTime}현재 웹서버 실행중 `);
+// });
