2675fccdd3090abe5d2e796d8ef02e32048fff0c
JungYiryung <yi_ryung@naver.com>
2025-04-11 13:56:34 +0900

    Fix: disconnectService에서 io빠진부분과 명세서에 안적힌 콜백 삭제

--- backend/socket/controller.js
+++ backend/socket/controller.js
@@ -13,7 +13,7 @@ export const socketController = (socket, io) => {
   socket.on("match-start", matchStartService(socket, io));
   socket.on("match-cancel", matchCancelService(socket, io));
   socket.on("room-outside", roomOutsideService(socket, io));
-  socket.on("disconnect", disconnectService(socket));
+  socket.on("disconnect", disconnectService(socket, io));
   socket.on("chat-message", chatMessageService(socket, io));
   socket.on("chat-typing", chatTypingService(socket, io));
   cron.schedule("0 0 0 * * *", midnightAlertService(socket, io));
